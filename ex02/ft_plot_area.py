ed6032bc345a36dd39c5487145c3ede3f7aff0b8
aelasefa <aymanelasefar1337@gmail.com>
2026-07-03 09:30:55 +0100

    Add initial implementations for garden-related functions

---
+++ ex02/ft_plot_area.py
@@ -0,0 +1,10 @@
+def ft_plot_area():
+    lenght = int(input("Enter length:"))    
+    width = int(input("Enter width:"))
+    print("Plot area:", lenght * width)
+
+def main():
+        ft_plot_area()
+
+if __name__ == "__main__":
+    main()
