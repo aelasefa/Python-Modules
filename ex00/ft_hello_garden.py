eb8e3abd6f6bb0e67bcf48e36b8b5aa44f79fdc1
ayelasefa <aymanelasefar1337@gmail.com>
2026-07-03 20:38:50 +0100

    put exercies in his dir

--- ex00/ft_hello_garden.py
+++
@@ -1,7 +0,0 @@
-def ft_hello_garden():
-    print("Hello, Garden Community!")
-
-def main():
-        ft_hello_garden()
-if  __name__ ==    "__main__":
-    main()
--- ex06/ft_count_harvest_iterative.py
+++
@@ -1,9 +0,0 @@
-def ft_count_harvest_iterative():
-    Days = int(input("Days until harvest: "))
-    for i in range(0, Days):
-        print(i + 1)
-    print("Harvest time!")
-def main():
-    ft_count_harvest_iterative()
-if __name__ == "__main__":
-    main()
